a5a897f2334bdc28b6ee8bbc45174b356af092c4
long1994 <longncgc00485@fpt.edu.vn>
2016-06-25 01:43:48 +0000

    Add files via upload

---
+++ BTVN buoi 3.py
@@ -0,0 +1,37 @@
+ bai 1
+##from turtle import*
+##color("yellow")
+##begin_fill()
+##for x in range(4):
+##    forward(100)
+##    left(90)
+
+bai 2
+##from turtle import*
+##color("yellow")
+##begin_fill()
+##for x in range(3):
+##    forward(100)
+##    left(120)
+
+bai 3
+##from turtle import*
+##color("yellow")
+##begin_fill()
+##for x in range(360):
+##    speed(0)
+##    forward(1)
+##    left(1)
+
+
+
+
+bai 4
+from turtle import*
+color("green")
+for i in range(6):
+    for x in range(360):
+        speed(0)
+        forward(1)
+        left(1)
+    left(60)
